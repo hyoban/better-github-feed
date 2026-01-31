1006b948ad740e1f86ace4863c63d893c534ca9c
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-31 13:56:01 +0800

    Update

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -1,4 +1,6 @@
 /// <reference path="../env.d.ts" />
 // For Cloudflare Workers, env is accessed via cloudflare:workers module
 // Types are defined in env.d.ts based on your alchemy.run.ts bindings
+// eslint-disable-next-line ts/ban-ts-comment
+// @ts-ignore
 export { env } from 'cloudflare:workers'
