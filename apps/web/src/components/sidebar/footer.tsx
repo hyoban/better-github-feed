b62807599812fe8f6d4e5af44d22a401f77eeda8
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-07-17 12:32:31 +0800

    Use product icon for repository link

--- apps/web/src/components/sidebar/footer.tsx
+++ apps/web/src/components/sidebar/footer.tsx
@@ -19,7 +19,7 @@ export function SidebarFooter() {
           />
         }
       >
-        <GitHubMark />
+        <ProductIcon />
       </Button>
       <SyncStatusIndicator compact />
       <UserMenu />
@@ -27,10 +27,6 @@ export function SidebarFooter() {
   )
 }
 
-function GitHubMark() {
-  return (
-    <svg viewBox="0 0 24 24" className="size-5" fill="currentColor" aria-hidden="true">
-      <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2.3c-3.3.7-4-1.4-4-1.4-.5-1.4-1.3-1.8-1.3-1.8-1.1-.7.1-.7.1-.7 1.2.1 1.8 1.2 1.8 1.2 1.1 1.8 2.8 1.3 3.5 1 .1-.8.4-1.3.8-1.6-2.7-.3-5.5-1.3-5.5-5.9 0-1.3.5-2.4 1.2-3.2-.1-.3-.5-1.5.1-3.2 0 0 1-.3 3.3 1.2a11.4 11.4 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.7.2 2.9.1 3.2.8.8 1.2 1.9 1.2 3.2 0 4.6-2.8 5.6-5.5 5.9.4.4.8 1.1.8 2.2v3.3c0 .3.2.7.8.6A12 12 0 0 0 12 .3Z" />
-    </svg>
-  )
+function ProductIcon() {
+  return <img src="/icon.svg" alt="" className="size-5" aria-hidden="true" />
 }
